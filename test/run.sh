feaef3a3496752113a9b0216dd0433f4155e96bc
Janne Valkealahti <janne.valkealahti@gmail.com>
2014-04-04 11:55:53 +0100

    reorganize and add maven

--- test/run.sh
+++ test/run.sh
@@ -2,16 +2,38 @@
 cd $(dirname $0)
 
 cd ../complete
-
 ./gradlew clean build
 ret=$?
 if [ $ret -ne 0 ]; then
   exit $ret
 fi
 
-cd ../initial
-
 ./gradlew clean
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+mvn clean
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+cd ../initial
+./gradlew projects
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+
+mvn clean
 ret=$?
 if [ $ret -ne 0 ]; then
   exit $ret
